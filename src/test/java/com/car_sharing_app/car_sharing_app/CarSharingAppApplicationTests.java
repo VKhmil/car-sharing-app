bd50b320aa2791689bd1849bac7d23b78ab1a374
VKhmil <vovahmil@gmail.com>
2024-10-04 18:44:43 +0300

    Pushing project to Git Hub remote repository

---
+++ src/test/java/com/car_sharing_app/car_sharing_app/CarSharingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.car_sharing_app.car_sharing_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSharingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
